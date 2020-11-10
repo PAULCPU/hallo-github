af5beb64c9f54d3e872fc696993e8170f4524ec7
Paul Strauch <strauchpaul4@gmail.com>
2020-11-10 20:45:35 +0100

    Fügt die initiale Solution hinzu

---
+++ Hallo-GitHub/Hallo-GitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hallo_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
